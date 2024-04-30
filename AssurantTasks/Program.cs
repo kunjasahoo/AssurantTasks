af77ce34952c11d4b157e535c8b109c65446b538
Kunja Bihari Sahoo <kusahoo@deloitte.com>
2024-04-30 17:46:14 +0530

    Add project files.

---
+++ AssurantTasks/Program.cs
@@ -0,0 +1,14 @@
+﻿using AssurantTasks.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((c, s) =>
+    {
+        s.AddSingleton<ITicTac, TicTac>();
+    })
+    .Build();
+
+var ticTac = builder.Services.GetRequiredService<ITicTac>();
+
+ticTac.PrintTicTac();
---
+++ AssurantTasks/Repository/TicTac.cs
@@ -0,0 +1,40 @@
+﻿namespace AssurantTasks.Repository
+{
+    internal class TicTac : ITicTac
+    {
+        public void PrintTicTac()
+        {
+            try
+            {
+                int tic = 3, tac = 5;
+                for (int i = 0; i <= 100; i++)
+                {
+                    if (i % tic == 0 && i % tac == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("{0 }{1,10} ", i, "TIC TAC");
+                    }
+                    else if (i % tic == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine(i + " TIC");
+                    }
+                    else if (i % tac == 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine(i + " TAC");
+                    }
+                    else Console.WriteLine(i);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error Message :" + ex.Message);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+        }
+    }
+}
